6b66ae8443fade79879790d19a996ef297e1e56f
Aster Taylor <astertaylor@uchicago.edu>
2022-07-21 18:59:05 -0700

    Added traj jump validation

--- SAMUS/examples/modularity_example.py
+++ SAMUS/examples/modularity_example.py
@@ -25,7 +25,7 @@ mod_class=SAMUS.model("modularity_test",mu=10**4)
 
 #runs simulation for a brief period with only 1 time step per rotation
 frame=mod_class.run_model(1,data_name='example_traj.txt',
-                          out_funcs=['moment_of_inertia','princ_axes',
+                          funcs=['moment_of_inertia','princ_axes',
                                      func,get_viscosity])
 
 #prints DataFrame as demonstration
